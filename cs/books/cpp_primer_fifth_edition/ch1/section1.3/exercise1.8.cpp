8e2045d52246a8040139c71170924653bf520bdf
zzy7675 <1044187675@qq.com>
2025-01-30 22:49:27 +0800

    complete section 1.3 exercise 1.7 and exercise 1.8

---
+++ cs/books/cpp_primer_fifth_edition/ch1/section1.3/exercise1.8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+* cancel the comment one by one, and compile 
+* to see the the result. This code is the
+* corrected version of statements for
+* Exercise 1.8
+*/
+int main()
+{
+    // std::cout << "/*";
+    // std::cout << "*/";
+    // std::cout << "/";
+    // std::cout << "" /* "" "/*" */;
+    return 0;
+}
