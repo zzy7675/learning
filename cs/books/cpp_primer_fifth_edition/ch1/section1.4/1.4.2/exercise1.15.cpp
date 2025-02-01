60ea55a66d74f0335cb27847f998dc04f24e954f
zzy7675 <1044187675@qq.com>
2025-02-01 21:04:26 +0800

    complete section 1.4.2 exercise 1.15

---
+++ cs/books/cpp_primer_fifth_edition/ch1/section1.4/1.4.2/exercise1.15.cpp
@@ -0,0 +1,18 @@
+/*
+* Exercise 1.15
+* This piece of code is intentially written to
+* have compile errors. Description of the error
+* is written in comments.
+*/
+
+// error: missing ) in parameter list for main
+int main ( {
+// error: used colon, not a semicolon, after endl
+std::cout << "Read each file." << std::endl:
+// error: missing quotes around string literal
+std::cout << Update master. << std::endl;
+// error: second output operator is missing
+std::cout << "Write new master." std::endl;
+// error: missing ; on return statement
+return 0
+}
